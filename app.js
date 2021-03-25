a606e941a246fe7f8e67a36901acec3a13bf554a
tyttseng <tyttseng@gmail.com>
2021-03-25 15:35:41 +0800

    add mysql & passport

--- app.js
+++ app.js
@@ -1,21 +1,89 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcryptjs')
 const methodOverride = require('method-override')
-
+const passport = require('passport')
 const app = express()
 const PORT = 3000
 
-app.engine('hbs', exphbs({ defaultLayout: 'main' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(methodOverride('_method'))
 
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
+const usePassport = require('./config/passport')
+usePassport(app)
+
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
+})
+
+// 資料相關路由
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+// 認證相關路由
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 app.listen(PORT, () => {
