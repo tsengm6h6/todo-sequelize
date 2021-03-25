ab643356da3379b6801c26bf1b33d41ec6f989b5
tyttseng <tyttseng@gmail.com>
2021-03-25 12:27:25 +0800

    project init

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcryptjs')
+const methodOverride = require('method-override')
+
+const app = express()
+const PORT = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
